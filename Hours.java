f0586aba670db2d62e09ab95a785c31d6fe6f759
ilhwi <155868342+ilhwi@users.noreply.github.com>
2024-03-06 23:21:59 +0900

    Update Hours.java
    horse 이름 수정 완료
    배팅기능 수정, 돈 따는 배율 추가

--- Hours.java
+++ Hours.java
@@ -3,27 +3,27 @@ package texst;
 import java.io.IOException;
 import java.util.Scanner;
 
-class HoursGame extends Thread{
-	Horse hoursRank = new Horse();
-	String hours;
-	private int num;
+class HorseGame extends Thread{ //쓰레드 상속
+	Horse hoursRank = new Horse(); // Horse안에 있는 스태틱 String 쓰기 위하여 객체생성
+	String hours; //말들
+	private int num; // 말들이 달리는 m
 
-	public HoursGame(String hours) {
+	public HorseGame(String hours) { //쓰레드 생성자
 		this.hours = hours;
 	}
 	
 	public void rank() {
-		for(int i=1;i<=Horse.hoursRank.length;i++) {
+		for(int i=1;i<=Horse.hoursRank.length;i++) { // 랭킹 나오게하는 출력문
 			if(Horse.hoursRank[i-1] != null) {
 			System.out.println(Horse.hoursRank[i-1]+"   "+i+"등");
 			}
 		}
 	}
-	public void Game() {
-		HoursGame hg = new HoursGame("날돌이");
-		HoursGame hg2 = new HoursGame("쌩쌩이");
-		HoursGame hg3 = new HoursGame("날씬이");
-		HoursGame hg4 = new HoursGame("빵빵이");
+	public void Game() { //본격 쓰레드 시작
+		HorseGame hg = new HorseGame("날돌이");
+		HorseGame hg2 = new HorseGame("쌩쌩이");
+		HorseGame hg3 = new HorseGame("날씬이");
+		HorseGame hg4 = new HorseGame("빵빵이");
 		hg.start();
 		hg2.start();
 		hg3.start();
@@ -33,12 +33,12 @@ class HoursGame extends Thread{
 			hg2.join();
 			hg3.join();
 			hg4.join();
-		}catch(InterruptedException e) {
+		}catch(InterruptedException e) { // 조인을 하지않으면 쓰레드작업이 끝나고 내가원하는 작업이 실행안되고 바로바로 됨
 			e.printStackTrace();
 		}
 	}
 	@Override
-	public void run() {
+	public void run() { //쓰레드 오버라이드 해주는거 내입맛대로
 		for(int i=0; i<1200; i++) {
 			num++;
 			if(num%200==0 || num%400==0 || num%800==0) {
@@ -57,10 +57,16 @@ class HoursGame extends Thread{
 		System.out.println("1 날돌이, 2. 썡쌩이, 3. 날씬이 4. 빵빵이");
 		System.out.print("1등할 말을 골라주세요 >>");
 		int user = scan.nextInt();
+		System.out.println("얼마 거실껀가요 ?");
+		int user_money = scan.nextInt();
 		Game();
 		rank();
-		if(Horse.hoursRank[0].equals(Horse.horseList[user]))
-			System.out.println("1등 맞췄습니다.");
+		if(Horse.hoursRank[0].equals(Horse.horseList[user-1])) {
+			user_money *= 2;
+			System.out.println("=======================");
+			System.out.println("= 배팅금액 : "+user_money+"$    =");
+			System.out.println("=======================");		
+		}
 		else {
 			System.out.println("==============");
 			System.out.println("$$ 돈 날라갔습니다.");
@@ -69,11 +75,11 @@ class HoursGame extends Thread{
 }
 
 public class Horse{
-	static String[] hoursRank = new String[10];
-	static String[] horseList = new String[] {"날돌이","쌩썡이","날씬이","빵빵이"};
+	static String[] hoursRank = new String[10]; // 랭킹시스템 10이지만 null값이 아닌거만 출력됨
+	static String[] horseList = new String[] {"날돌이","쌩썡이","날씬이","빵빵이"}; // 내말 리스트 랭킹이랑 비교할려구
 	static int rank = 0;
 	public static void main(String[] args) {
-		HoursGame hg = new HoursGame("시작");
+		HorseGame hg = new HorseGame("시작"); // 객체생성을 위해 햇음
 		hg.GameStart();
 
 	}
